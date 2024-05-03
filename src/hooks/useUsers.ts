b7b8e3d75cf7e02f3223910acc3da1f241192a8c
Filip Kozik <536410@mail.muni.cz>
2024-05-03 15:28:29 +0200

    fix: hook typos

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,58 @@
+import { CreateUser, UpdateUser } from "../api/types";
+import UserApi from "../api/userApi";
+import "../api/baseApi";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+export const useUser = (id: string) => {
+  const { data } = useQuery({
+    queryKey: ["product"],
+    queryFn: () => UserApi.getSingle(id),
+  });
+
+  return { data };
+};
+
+export const useUsers = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: () => UserApi.getAll(),
+  });
+
+  return { data, isLoading };
+};
+
+export const useUserCreate = () => {
+  const queryClient = useQueryClient();
+  const { mutateAsync } = useMutation({
+    mutationFn: (payload: CreateUser) => UserApi.createSingle(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+    },
+  });
+
+  return { mutateAsync };
+};
+
+export const useUserUpdate = (id: string) => {
+  const queryClient = useQueryClient();
+  const { mutateAsync } = useMutation({
+    mutationFn: (payload: UpdateUser) => UserApi.updateSingle(id, payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+    },
+  });
+
+  return { mutateAsync };
+};
+
+export const useUserDelete = (id: string) => {
+  const queryClient = useQueryClient();
+  const { mutateAsync } = useMutation({
+    mutationFn: () => UserApi.deleteSingle(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+    },
+  });
+
+  return { mutateAsync };
+};
